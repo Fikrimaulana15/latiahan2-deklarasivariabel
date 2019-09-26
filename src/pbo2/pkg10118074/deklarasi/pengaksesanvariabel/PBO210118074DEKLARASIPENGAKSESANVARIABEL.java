598c444c3ef7e7d2c03eeaea7caf408f8c0e4504
fikrimaualana <55828126+Fikrimaulana15@users.noreply.github.com>
2019-09-26 15:16:05 +0700

    selesai

---
+++ src/pbo2/pkg10118074/deklarasi/pengaksesanvariabel/PBO210118074DEKLARASIPENGAKSESANVARIABEL.java
@@ -0,0 +1,43 @@
+///*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118074.deklarasi.pengaksesanvariabel;
+
+/**
+ *
+ * @author 
+ * NAMA : FikriMaulana
+ * KELAS : IF-2
+ * NIM : 10118074
+ */
+public class PBO210118074DEKLARASIPENGAKSESANVARIABEL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Deklarasi variabel
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memberi nilai ke variabel
+        nilaiInt= 78;
+        nilaiLogika =false;
+        nilaiKarakter='D';
+        
+        //Menampilakan hasil
+        System.out.println();
+        System.out.println("isi variabel nilai=" + nilaiInt);
+        System.out.println("isi variabel PHI= " + PHI);
+        System.out.println("isi variabel logika= " + nilaiLogika);
+        System.out.println("isi variabel karakter= " + nilaiKarakter);
+        
+    }
+    
+}
